9c70af909424dd0b4a5e412f84fa50ce34dd9cd7
yushoyamaguchi <ysh.824@outlook.jp>
2022-05-01 18:10:55 +0900

    declear

--- emu.rs
+++ emu.rs
@@ -2,6 +2,21 @@ use std::env;
 use std::process;
 use std::fs;
 
+enum Register { EAX=0, ECX, EDX, EBX, ESP, EBP, ESI, EDI }
+const REGISTERS_COUNT: usize = 8;
+const REGISTERS_NAME: [&str; REGISTERS_COUNT] = ["EAX", "ECX", "EDX", "EBX", "ESP", "EBP", "ESI", "EDI"];
+
+const MEMORY_SIZE: usize = 1024 * 1024;
+struct Emulator {
+    // General-purpose Registers
+    registers: [u32; REGISTERS_COUNT],
+    // EFLAGS Register
+    eflags: u32,
+    // Instruction Pointer
+    eip: u32,
+    // Memory
+    memory: Vec<u8>
+}
 
 fn main(){
     let args: Vec<String> = env::args().collect();
